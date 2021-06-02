af9eaa56992a8213786e66d0ce6cb7ca7879399c
i-smolina <smolirinka@gmail.com>
2021-06-02 15:17:52 +0500

    created empty Spring Project

---
+++ SpringTestTask/src/test/java/ru/smolina/SpringTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.smolina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
